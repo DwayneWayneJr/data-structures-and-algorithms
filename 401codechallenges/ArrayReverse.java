cf10f6de46074724200b33b1f87fa0be43a03afd
DwayneWayneJr <dwaynejgrant@gmail.com>
2020-01-07 10:43:12 -0800

    added assets folder

--- 401codechallenges/ArrayReverse.java
+++ 401codechallenges/ArrayReverse.java
@@ -1,7 +1,8 @@
+import java.util.Arrays;
 public class ArrayReverse {
-  public static void main(String[] args) {
+  public static void main (String[] args) {
     // create the array to test your code on later
-    int[] startArr = new int[] { 1, 2, 3, 4, 5, 10 };
+    int[] startArr = new int[] {1,2,3,4,5,10};
     // print it out, nicely
     System.out.println(Arrays.toString(startArr));
     // call your reverseArray method and save the result in a variable
@@ -9,9 +10,7 @@ public class ArrayReverse {
     // print out the result, nicely
     System.out.println(Arrays.toString(endArr));
   }
-
   // the method you should write, to reverse an array
-  public static int[] reverseArray(int[] inputArray) {
   public static int[] reverseArray(int[] arr) {
     int placeHolder;
     for(int i = 0; i<arr.length/2; i++){
@@ -19,5 +18,5 @@ public class ArrayReverse {
       arr[i] = arr[arr.length-i-1];
       arr[arr.length-i-1] = placeHolder;
     } return arr;
-  }
+  } 
 }
