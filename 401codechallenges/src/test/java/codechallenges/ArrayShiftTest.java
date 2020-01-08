cb4b88689227455db3f7fde60e1a37102d31ecaa
DwayneWayneJr <dwaynejgrant@gmail.com>
2020-01-08 10:11:06 -0800

    code chal branch for submission

--- 401codechallenges/src/test/java/codechallenges/ArrayShiftTest.java
+++ 401codechallenges/src/test/java/codechallenges/ArrayShiftTest.java
@@ -7,6 +7,6 @@ import static org.junit.Assert.*;
 public class ArrayShiftTest {
 
     @Test public void testShiftArrayCorrectAns () {
-        assertEquals("this is going to be correct", [2,4,5,6,8], );
+        assertEquals("this is going to be correct", [2,4,5,6,8], [],);
     }
 }
