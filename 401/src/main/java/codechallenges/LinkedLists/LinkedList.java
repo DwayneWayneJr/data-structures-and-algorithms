d6f2a94f21cbef12f6733648ea3d3d1a620093a1
DwayneWayneJr <dwaynejgrant@gmail.com>
2020-01-15 12:07:25 -0800

    insert new complete

--- 401/src/main/java/codechallenges/LinkedLists/LinkedList.java
+++ 401/src/main/java/codechallenges/LinkedLists/LinkedList.java
@@ -1,29 +1,34 @@
 package codechallenges.LinkedLists;
 
 public class LinkedList {
+    // head of list
     Node head;
 
+    // Node class
     public static class Node {
         int data;
         Node next;
+
+//   Constructor to create a new node
         Node(int d){
             data = d;
             next = null;
         }
     }
 
-    public void insert(int value) {
-        Node newNode = new Node(value);
+//   Inserts a new Node at front of the list.
+    public void insertNew(int new_data) {
+        Node new_node = new Node(new_data);
         if (this.head == null) {
-            this.head = newNode;
+            this.head = new_node;
         } else {
-            head.next = newNode;
+            head.next = new_node;
         }
     }
 
-    public void includes(int value) {
-        Node head = this.head;
-        while  (head != null) {
+    public boolean includesValue(int data) {
+        Node current_Node = this.head;
+        while  (current_ != null) {
             if (head.data == value) {
                 return;
             }
