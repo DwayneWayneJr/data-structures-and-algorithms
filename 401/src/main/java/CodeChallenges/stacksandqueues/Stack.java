be6a2130a2f297bc7f6dadcb5cf555f825b62f2b
DwayneWayneJr <dwaynejgrant@gmail.com>
2020-01-20 13:13:59 -0800

    working in stack

--- 401/src/main/java/CodeChallenges/stacksandqueues/Stack.java
+++ 401/src/main/java/CodeChallenges/stacksandqueues/Stack.java
@@ -1,5 +1,13 @@
 package codechallenges.stacksandqueues;
 
 public class Stack {
+    Node top;
 
+    public void setTop(Node top) {
+        this.top = null;
+    }
+
+    public void pushItToStack(String NodeValue) {
+        Node nodeValue = new Node
+    }
 }
