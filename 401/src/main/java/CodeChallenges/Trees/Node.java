9b5fbe32b19d7cc0d471a59907e5d669cc4455fd
DwayneWayneJr <dwaynejgrant@gmail.com>
2020-01-28 10:02:17 -0800

    challenge not done

--- 401/src/main/java/CodeChallenges/Trees/Node.java
+++ 401/src/main/java/CodeChallenges/Trees/Node.java
@@ -1,4 +1,10 @@
 package codechallenges.Trees;
 
 public class Node {
+    int key;
+    Node left, right;
+
+    public Node(int stored) {
+
+    }
 }
--- 401/src/main/java/CodeChallenges/Trees/Tree.java
+++ 401/src/main/java/CodeChallenges/Trees/Tree.java
@@ -1,4 +1,18 @@
 package codechallenges.Trees;
 
+
+
 public class Tree {
+    Node root;
+
+    Tree() {
+        root = null;
+    }
+
+    void preOrder(Node node) {
+        if (node == null)
+            return;
+
+        printPreorder(node.left);
+    }
 }
